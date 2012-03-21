d33288a8d377925b330fbc01669436cff189cfde
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2012-03-20 23:51:51 -0400

    consolidate into a single .ex file

--- src/elixir_cowboy.ex
+++ src/elixir_cowboy.ex
@@ -21,3 +21,33 @@ defmodule :elixir_cowboy do
 	end
 end
 
+defmodule :elixir_cowboy_handler do
+	@behavior :cowboy_http_handler
+
+	def init({_, :http}, req, []) do
+		{:ok, req, :undefined}
+	end
+
+	def handle(req, state) do
+		{:ok, req2} = Erlang.cowboy_http_req.reply(200, [], "Hello world!", req)
+		{:ok, req2, state}
+	end
+
+	def terminate(_, _) do
+		:ok
+	end
+	
+end
+
+defmodule :elixir_cowboy_sup do
+	@behavior :supervisor
+
+	def start_link() do
+		Erlang.supervisor.start_link({:local, :elixir_cowboy_sup}, :elixir_cowboy_sup, [])
+	end
+
+	def init([]) do
+		{:ok, {{:one_for_one, 10, 10}, []}}
+	end
+end
+
